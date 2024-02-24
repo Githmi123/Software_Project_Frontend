73b858fddd9cb2c267687f80eb7c058f89639cd1
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-02-24 23:41:48 +0530

    Completed the UI design for the left paneof the User Profile Page.

--- software_project_frontend/src/components/UserProfileLeftPane/UserProfileLeftPane.jsx
+++ software_project_frontend/src/components/UserProfileLeftPane/UserProfileLeftPane.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './UserProfileLeftPane.css';
 import ring from '../../images/ring.png'
 import user1 from '../../images/user1.png'
+import stars from '../../images/stars.png'
 
 export const UserProfileLeftPane = () => {
     return(
         <div className="LeftPane">
-            <div id='container'>
+            
+            <div id='container1'>
+                
                 <div id='ringcontainer'>
                     <img src={ring} id='ring'></img>
                     <img src={user1} id = 'user1'></img>
@@ -18,22 +21,33 @@ export const UserProfileLeftPane = () => {
                     <p>Driving excellence in education with cutting-edge technology</p>
                     
                 </div>
-                <button id="editProfile">
+                <button className="greytext">
                     <i className="fas fa-pencil-alt"></i> 
                     
                     <span>Edit Profile</span>
                 </button>
 
                 
-            
-            
-
-
-            </div>
-
-            <div>
                 
+            
+
+
             </div>
+            
+            <div id='container2'>
+                <h2>DAVID PERERA</h2>
+                <span className='greytext'>Senior Lecturer</span>
+                <i className="fas fa-envelope"></i>
+                <span className='greytext'>davidperera123@gmail.com</span>
+                <i className="fas fa-phone-alt"></i>
+                <span className='greytext'>+94772452625</span>
+                <i className="fas fa-map-marker-alt"></i>
+                <span className='greytext'>No. 47/220, Lake street,
+                    Colombo 07</span>
+                
+
+            </div>
+            <img src={stars} id = 'stars'></img>
             
 
         </div>
