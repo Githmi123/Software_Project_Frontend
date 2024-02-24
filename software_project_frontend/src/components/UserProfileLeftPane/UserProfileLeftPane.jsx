6c922a0c1498d9952fcc455445a4b160618fc13e
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-02-24 16:20:44 +0530

    Added the top section of the left pane of user profile page.

--- software_project_frontend/src/components/UserProfileLeftPane/UserProfileLeftPane.jsx
+++ software_project_frontend/src/components/UserProfileLeftPane/UserProfileLeftPane.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import './UserProfileLeftPane.css';
 import ring from '../../images/ring.png'
+import user1 from '../../images/user1.png'
 
 export const UserProfileLeftPane = () => {
     return(
         <div className="LeftPane">
             <div id='container'>
-            <img src={ring} id='ring'></img>
+                <div id='ringcontainer'>
+                    <img src={ring} id='ring'></img>
+                    <img src={user1} id = 'user1'></img>
+                    
+            
+                </div>
+                <div id='text'>
+                    <h1 >Helllo David !</h1>
+                    <p>Driving excellence in education with cutting-edge technology</p>
+                    
+                </div>
+                <button id="editProfile">
+                    <i className="fas fa-pencil-alt"></i> 
+                    
+                    <span>Edit Profile</span>
+                </button>
+
+                
+            
+            
 
 
+            </div>
+
+            <div>
+                
             </div>
             
 
