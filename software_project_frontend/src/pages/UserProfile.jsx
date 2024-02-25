4a2fe3a43f230d0e374a23ef09db35b7e04d55e6
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-02-25 14:23:16 +0530

    Made the changes for the UserProfile page to add the User Profile Right Pane.

--- software_project_frontend/src/pages/UserProfile.jsx
+++ software_project_frontend/src/pages/UserProfile.jsx
@@ -2,20 +2,22 @@
 // import './Login.css';
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import '../styles/UserProfile.css'
 import "../components/RightPane/RightPane.css"
 
 import { LeftPane } from '../components/LeftPane/LeftPane';
 import { RightPane } from '../components/RightPane/RightPane';
 import { UserProfileLeftPane } from '../components/UserProfileLeftPane/UserProfileLeftPane';
+import { UserProfileRightPane } from '../components/UserProfileRightPane/UserProfileRightPane';
 
 
 function UserProfile() {
 
 
   return (
-    <div  style={{backgroundColor: "#7894DB"}}>
+    <div id='body'>
       <UserProfileLeftPane/>
+      <UserProfileRightPane/>
   
        
         
