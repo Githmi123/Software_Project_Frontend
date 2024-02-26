2cd13aab652be607d7ec98e84ebac92f76a4a4e9
kalangijayakody <kalangijayakody1002@gmail.com>
2024-02-26 10:30:30 +0530

    Page layout

--- software_project_frontend/src/components/MainRightPane/MainRightPane.jsx
+++ software_project_frontend/src/components/MainRightPane/MainRightPane.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import '../MainRightPane/MainRightPane.css'
 import { Box, Button } from '@mui/material'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+
 //import SearchBar from './SearchBar';
 
 
-const MainRightPane = () => {
+const MainRightPane = ({ children }) => {
   return (
     <div className='maindiv-right'>
       
-      <div className='container' style={{margin:'40px 40px 40px auto'}}>    
-        <Button  sx={{m:2, width:'100px', height:'50px',color:'black',fontWeight:'bold' }} startIcon={<ArrowBackIcon/>}>Home</Button>
+      <div className='container' style={{margin:'40px 40px 40px auto'}}>   
+      {children} 
+        
         
       </div>
 
--- software_project_frontend/src/pages/RecentPage.jsx
+++ software_project_frontend/src/pages/RecentPage.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import MainLeftPane from '../components/MainLeftPane/MainLeftPane'
 import MainRightPane from '../components/MainRightPane/MainRightPane'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { Button } from '@mui/material';
 
 const RecentPage = () => {
   return (
     <div>
       <MainLeftPane/>
-      <MainRightPane/>
+      <MainRightPane >
+      <Button  sx={{m:2, width:'100px', height:'50px',color:'black',fontWeight:'bold' }} startIcon={<ArrowBackIcon/>}>Home</Button>
+        <h1>Recent</h1>
+      </MainRightPane>
     </div>
   )
 }
