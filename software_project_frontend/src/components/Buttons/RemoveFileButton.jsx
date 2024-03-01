a22c99a95c725e9689518824a0c8c63ab5092c8b
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-03-01 14:00:47 +0530

    Created a seperate component for the Remove FIle button.

---
+++ software_project_frontend/src/components/Buttons/RemoveFileButton.jsx
@@ -0,0 +1,21 @@
+import { Button } from '@mui/material'
+import CancelIcon from '@mui/icons-material/Cancel';
+import React from 'react'
+
+const RemoveFileButton = () => {
+  return (
+    <div>
+        <Button
+            variant='contained'
+            style={{margin:"10px", width : "20vh", textTransform: "capitalize", backgroundColor: "#ED8484", borderRadius:"3vh"}}
+            startIcon = {<CancelIcon style={{color:"black"}}/>}
+            // onClick={onClick}
+        > 
+            Remove File
+        </Button>
+      
+    </div>
+  )
+}
+
+export default RemoveFileButton;
