69543c416da790a2356b4be1f40e1a763772b118
kalangijayakody <kalangijayakody1002@gmail.com>
2024-03-02 20:31:24 +0530

    Add styles to MyModulesPage

--- software_project_frontend/src/App.js
+++ software_project_frontend/src/App.js
@@ -10,14 +10,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.css';
 import RecentPage from './pages/RecentPage';
 import MyModulesPage from './pages/MyModulesPage';
-<<<<<<< HEAD
 import NewModule from './pages/NewModule';
-=======
 import NewAssignmentPage from './pages/NewAssignmentPage';
 import BatchesPage from './pages/BatchesPage';
 import AssignmentsPage from './pages/AssignmentsPage';
 import AnswerScriptsPage from './pages/AnswerScriptsPage';
->>>>>>> 7f4f0c8a692163638845c479283ec5394dd7cadb
+import ManualGradingPage from './pages/ManualGradingPage';
 
 
 function App() {
@@ -30,14 +28,12 @@ function App() {
         <Route exact path="/SignUp" element={<SignUp/>}></Route>
         <Route exact path="/RecentPage" element={<RecentPage/>}></Route>
         <Route exact path="/MyModulePage" element={<MyModulesPage/>}></Route>
-<<<<<<< HEAD
         <Route exact path="/NewModule" element={<NewModule/>}></Route> 
-=======
         <Route exact path="/NewAssignment" element={<NewAssignmentPage/>}></Route>
         <Route exact path="/Batches" element={<BatchesPage/>}></Route>
         <Route exact path="/Assignments" element={<AssignmentsPage/>}></Route>
         <Route exact path="/AnswerScripts" element={<AnswerScriptsPage/>}></Route>
->>>>>>> 7f4f0c8a692163638845c479283ec5394dd7cadb
+        <Route exact path="/ManualGradingPage" element={<ManualGradingPage/>}></Route>
         {/* Define other routes as needed */}
       </Routes>
     </Router>
