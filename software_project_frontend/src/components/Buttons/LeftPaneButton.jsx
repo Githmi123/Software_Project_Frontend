771e1259603e0ca06eba1010be784ba850dc685f
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-03-14 07:20:00 +0530

    Initially created a LeftPaneButton component.

--- software_project_frontend/src/components/Buttons/LeftPaneButton.jsx
+++ software_project_frontend/src/components/Buttons/LeftPaneButton.jsx
@@ -1,35 +1,31 @@
-import { Button } from '@mui/material'
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import DashboardIcon from '@mui/icons-material/Dashboard';
+// import { Button } from '@mui/material'
+// import React, { useState } from 'react'
+// import { Link } from 'react-router-dom'
+// import DashboardIcon from '@mui/icons-material/Dashboard';
 
-function LeftPaneButton({ icon: Icon, name, link }) {
-    const [selectedButton, setSelectedButton] = useState(false);
+// function LeftPaneButton({ icon: Icon, name, link }) {
+    
 
-    const handleSelectedButton = () => {
-        setSelectedButton(true);
-    }
+//     const buttonStyle = {
+//         backgroundColor: selectedButton ? '#ff0000' : 'initial', // Change to the color you want
+//         color: selectedButton ? 'white' : 'black', // Change to the color you want
+//         display: 'flex',
+//         justifyContent: 'flex-start',
+//         textAlign: 'left',
+//         width: '100%',
+//     };
 
-    const buttonStyle = {
-        backgroundColor: selectedButton ? '#ff0000' : 'initial', // Change to the color you want
-        color: selectedButton ? 'white' : 'black', // Change to the color you want
-        display: 'flex',
-        justifyContent: 'flex-start',
-        textAlign: 'left',
-        width: '100%',
-    };
+//     return (
+//         <Button
+//             className="button"
+//             component={Link} to={link}
+//             startIcon={<Icon />}
+//             sx={buttonStyle}
+//             onClick={handleSelectedButton}
+//         >
+//             {name}
+//         </Button>
+//     )
+// }
 
-    return (
-        <Button
-            className="button"
-            component={Link} to={link}
-            startIcon={<Icon />}
-            sx={buttonStyle}
-            onClick={handleSelectedButton}
-        >
-            {name}
-        </Button>
-    )
-}
-
-export default LeftPaneButton
+// export default LeftPaneButton
