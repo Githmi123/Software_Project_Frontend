3ad4d28263f5c1c6b915431885ea3285aa71e9f5
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-03-14 17:03:44 +0530

    Altered the styles of the Login button.

--- software_project_frontend/src/components/Buttons/LogInButton.jsx
+++ software_project_frontend/src/components/Buttons/LogInButton.jsx
@@ -11,37 +11,40 @@ function LogInButton() {
  
   return (
     <div> 
-      <Stack spacing={2} direction="row">
+      <Stack spacing={2} direction="row" >
       
         <Button  
       variant="text"
       type='submit'
       style={{
-        position: 'absolute',
-        left: '45%',
-        right: '0%',
-        // top: '5%',
-        bottom: '15%',
+        position: 'relative',
+        // left: '45%',
+        // right: '0%',
+        // // top: '5%',
+        // bottom: '15%',
         background: '#7894DB',
         borderRadius: '12px',
         height: '6%',
-        width: '12%'
+        display: 'flex',
+          alignItems: 'center',
+          justifyContent: "center",
+          flexDirection: "row",
+                  width: 'auto'
       }}
     >
-      <img src={login} alt="rs" style={{ height: "15px", width: "auto", position: "absolute", left: "15px", top: "10px"}}/>
+      <img src={login} alt="rs" style={{ height: "15px", width: "auto", position: "relative"}}/>
       <div style={{
-          position: 'absolute',
-          left: '35.33%',
-          right: '27.33%',
-          top: '20%',
-          bottom: '29.09%',
+          position: 'relative',
+          // left: '35.33%',
+          // right: '27.33%',
+          // top: '20%',
+          // bottom: '29.09%',
           fontFamily: 'Roboto',
           fontStyle: 'normal',
           fontWeight: 700,
           fontSize: '15px',
           lineHeight: '23px',
-          display: 'flex',
-          alignItems: 'center',
+          
           color: '#FFFFFF',
         }}>
           
