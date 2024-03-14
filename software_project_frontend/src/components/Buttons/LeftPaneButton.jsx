ebbaa69ac6f10b4881e8b2ad35dde92a1c5e4ec9
githmisundarasekara@gmail.comGithmi123 <githmisundarasekara@gmail.com>
2024-03-14 07:17:43 +0530

    Created a LeftPaneButton component.

---
+++ software_project_frontend/src/components/Buttons/LeftPaneButton.jsx
@@ -0,0 +1,35 @@
+import { Button } from '@mui/material'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+function LeftPaneButton({ icon: Icon, name, link }) {
+    const [selectedButton, setSelectedButton] = useState(false);
+
+    const handleSelectedButton = () => {
+        setSelectedButton(true);
+    }
+
+    const buttonStyle = {
+        backgroundColor: selectedButton ? '#ff0000' : 'initial', // Change to the color you want
+        color: selectedButton ? 'white' : 'black', // Change to the color you want
+        display: 'flex',
+        justifyContent: 'flex-start',
+        textAlign: 'left',
+        width: '100%',
+    };
+
+    return (
+        <Button
+            className="button"
+            component={Link} to={link}
+            startIcon={<Icon />}
+            sx={buttonStyle}
+            onClick={handleSelectedButton}
+        >
+            {name}
+        </Button>
+    )
+}
+
+export default LeftPaneButton
