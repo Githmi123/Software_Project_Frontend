762fa34f607e34f59c91369f56bc323f0c03bf84
kalangijayakody <kalangijayakody1002@gmail.com>
2024-04-04 16:17:03 +0530

    Connected backend with posting a new module and getting module functionalities.

--- software_project_frontend/src/pages/NewBatchPage.jsx
+++ software_project_frontend/src/pages/NewBatchPage.jsx
@@ -1,53 +1,60 @@
-import React, { useState } from 'react'
-import { TextField ,Button} from '@mui/material'
-import MainLeftPane from '../components/MainLeftPane/MainLeftPane'
-import MainRightPane from '../components/MainRightPane/MainRightPane'
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import CustomButton from '../components/Buttons/CustomButton';
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
+import MainLeftPane from "../components/MainLeftPane/MainLeftPane";
+import MainRightPane from "../components/MainRightPane/MainRightPane";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import CustomButton from "../components/Buttons/CustomButton";
 
-import '../styles/NewAssignmentPage.css'
-import "../styles/NewBatchPage.css"
+import "../styles/NewAssignmentPage.css";
+import "../styles/NewBatchPage.css";
 
 const NewBatchPage = () => {
+  const [newBatch, setNewBatch] = useState(null);
 
-    const [selectNewBatch,setSelectedNewBatch] = useState(null);
-
-    const handleNewBatch= () => {
-
-
-    }
+  const handleNewBatch = () => {};
 
   return (
-    <div className = "align1">
-               <MainLeftPane/>
-               <MainRightPane>
-               <Button  sx={{m:2, width:'100px', height:'50px',color:'black',fontWeight:'bold' }} startIcon={<ArrowBackIcon/>} onClick={() => window.history.back()} >Home</Button>
+    <div className="align1">
+      <MainLeftPane />
+      <MainRightPane>
+        <Button
+          sx={{
+            m: 2,
+            width: "100px",
+            height: "50px",
+            color: "black",
+            fontWeight: "bold",
+          }}
+          startIcon={<ArrowBackIcon />}
+          onClick={() => window.history.back()}
+        >
+          Home
+        </Button>
 
-               <h1 id='heading-new-batch-page' >New Batch</h1>
-               <div id='textfield-set'>
-               <span id='label1-new-batch-page'>Batch Number</span>
-                <TextField
-                    id="outlined-basic"
-                    label="Batch Number"
-                    variant="outlined"
-                    //value={assignmentName}
-                    //onChange={handleAssignmentNameChange}
-                    sx={{ m: '2vh', width:"100vh", maxWidth: '70vh', padding:"0", position:"relative"}}
-                />
-
-               </div>
-               <Button id='add-button' onClick={handleNewBatch} variant='contained'>
-                    Add
-                </Button>
-
-              
-               </MainRightPane>
-        
-        
-
-      
+        <h1 id="heading-new-batch-page">New Batch</h1>
+        <div id="textfield-set">
+          <span id="label1-new-batch-page">Batch Number</span>
+          <TextField
+            id="outlined-basic"
+            label="Batch Number"
+            variant="outlined"
+            //value={assignmentName}
+            //onChange={handleAssignmentNameChange}
+            sx={{
+              m: "2vh",
+              width: "100vh",
+              maxWidth: "70vh",
+              padding: "0",
+              position: "relative",
+            }}
+          />
+        </div>
+        <Button id="add-button" onClick={handleNewBatch} variant="contained">
+          Add
+        </Button>
+      </MainRightPane>
     </div>
-  )
-}
+  );
+};
 
-export default NewBatchPage
+export default NewBatchPage;
