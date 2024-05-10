3e2a9f74014d5eb3270a479c14f2696ca6b443b0
KalangiJayakody <thathsarakalangi@gmail.com>
2024-05-10 20:38:39 +0530

    Connected the backend with the functionality of creating a new assignment

--- software_project_frontend/src/components/Buttons/InputFileUploadButton.jsx
+++ software_project_frontend/src/components/Buttons/InputFileUploadButton.jsx
@@ -1,64 +1,50 @@
-import React, { useState } from 'react';
-import { styled } from '@mui/material/styles';
-import { Button, InputAdornment, TextField } from '@mui/material';
-// import Button from '@mui/material/Button';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
+import { Button, InputAdornment, TextField } from "@mui/material";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
-const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
   height: 1,
-  overflow: 'hidden',
-  position: 'relative',
+  overflow: "hidden",
+  position: "relative",
   bottom: 0,
   left: 0,
-  whiteSpace: 'nowrap',
+  whiteSpace: "nowrap",
   width: 1,
 });
 
-const InputFileUploadButton = ({onFileSelect}) => {
+const InputFileUploadButton = ({ onFileSelect }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    // const [selectedFile, setSelectedFile] = useState(null);
-
-    const handleFileSelect = (event) => {
-        if (event.target && event.target.files && event.target.files.length > 0) {
-            const file = event.target.files[0];
-            // setSelectedFile(file);
-            onFileSelect(file);
-        }
-    };
-    
+  const handleFileSelect = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      setSelectedFile(file);
+      onFileSelect(file); // Pass the selected file to the parent component
+    }
+  };
 
   return (
-    
     <>
-        <Button
-            component="label"
-            role={undefined}
-            variant="contained"
-            tabIndex={-1}
-            // type='image'
-            //   startIcon={<CloudUploadIcon />}
-            style={{ margin: "0", borderRadius: 5}}
-        >
-            <CloudUploadIcon />
-            <VisuallyHiddenInput type="file" accept="image/*" onChange={handleFileSelect}/>
-        </Button>
-        {/* {selectedFile && (
-                <TextField
-                    value={selectedFile.name}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">Selected: </InputAdornment>
-                        ),
-                    }}
-                    variant="outlined"
-                    fullWidth
-                    disabled
-                />
-        )} */}
+      <Button
+        component="label"
+        role={undefined}
+        variant="contained"
+        tabIndex={-1}
+        style={{ margin: "0", borderRadius: 5 }}
+        startIcon={<CloudUploadIcon />}
+      >
+        Upload
+        <VisuallyHiddenInput
+          type="file"
+          accept=".xlsx, .xls, .csv"
+          onChange={handleFileSelect}
+        />
+      </Button>
     </>
   );
-}
+};
 
 export default InputFileUploadButton;
--- software_project_frontend/src/pages/BatchesPage.jsx
+++ software_project_frontend/src/pages/BatchesPage.jsx
@@ -6,14 +6,12 @@ import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import AddCircleIcon from "@mui/icons-material/AddCircle";
 import CustomNewButton from "../components/Buttons/CustomNewButton";
 
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import refreshAccessToken from "../services/AuthService";
 import axios from "axios";
 import Cookies from "js-cookie";
 
 import "../styles/BatchesPage.css";
-
-//const batches = ["22th batch", "23rd batch", "24th batch", "25th batch"];
 
 const BatchesPage = () => {
   const { selectedModuleCode } = useParams();
@@ -23,6 +21,39 @@ const BatchesPage = () => {
   const [moduleData, setModuleData] = useState(null);
 
   //console.log(selectedModuleCode);
+
+  // useEffect(() => {
+  //   const fetchBatches = async () => {
+  //     try {
+  //       const accessToken = Cookies.get("accessToken");
+  //       if (!accessToken) {
+  //         console.error("Access token not available");
+  //       }
+
+  //       const config = {
+  //         headers: {
+  //           Authorization: `Bearer ${accessToken}`,
+  //         },
+  //       };
+
+  //       const response = await axios.get(
+  //         `http://localhost:3500/batch/${selectedModuleCode}`,
+  //         config
+  //       );
+  //       setBatches(response.data);
+  //       console.log("batches", response.data);
+  //       //console.log(selectedModuleCode);
+  //       setIsLoading(false);
+  //     } catch (error) {
+  //       console.error("Error fetching batches:", error);
+  //       setIsLoading(false);
+  //     }
+  //   };
+
+  //   if (selectedModuleCode) {
+  //     fetchBatches();
+  //   }
+  // }, [selectedModuleCode]);
 
   useEffect(() => {
     async function fetchData() {
@@ -41,15 +72,13 @@ const BatchesPage = () => {
         //console.log(selectedModuleCode);
         setIsLoading(false);
       } catch (error) {
-        console.error("Error fetching batches:", error);
-        setIsLoading(false);
+        console.error("Error fetching module data:", error);
       }
     }
 
-    if (selectedModuleCode) {
-      fetchBatches();
-    }
-  }, [selectedModuleCode]);
+    fetchData();
+  }, []);
+
   const handleNewBatch = (event) => {};
 
   const handleSelectedBatch = (batch) => {
@@ -99,11 +128,25 @@ const BatchesPage = () => {
         >
           Home
         </Button>
-        <h1 id="heading">Module</h1>
+        {/* Check if moduleData is available before rendering */}
+        <h1 id="heading">
+          {moduleData ? (
+            <>
+              {moduleData.modulecode} - {moduleData.modulename}
+            </>
+          ) : (
+            "Loading..."
+          )}
+        </h1>
         <div>
           <Link to={`/NewBatchPage/${selectedModuleCode}`}>
-            <CustomNewButton text="New Batch" onClick={handleNewBatch} />
+            <CustomNewButton text="New Batch" />
           </Link>
+          <Link to={`/EditModule/${selectedModuleCode}`}>
+            <CustomNewButton text="Edit Module" />
+          </Link>
+
+          <CustomNewButton text="Delete Module" />
         </div>
 
         <div className="column">
@@ -114,7 +157,7 @@ const BatchesPage = () => {
           ) : (
             batches.map((batch) => (
               <Link
-                to={`/Assignments/${batch.batchId}`}
+                to={`/Assignments/${selectedModuleCode}/${batch.batch}`}
                 key={batch.batchId}
                 style={{ textDecoration: "none", color: "inherit" }}
               >
@@ -136,28 +179,6 @@ const BatchesPage = () => {
             ))
           )}
         </div>
-
-        {/* <div id='module-table'>
-                    <table>
-                        <tr id='module-table-headers' sx={{fontWeight:'bolder'}}>
-                        <th>Module Code</th>
-                        <th>Module Name</th>
-                        </tr>
-                        {table_data_modules.map((val,key)=>{
-                        return(
-                            <tr key={key} style={{backgroundColor:'#E3DDE8', borderRadius:'30px', margin:'0 0 5px 0'}}>
-                            <td>{val.Module_Code}</td>
-                            <td>{val.Module_Name}</td>
-                            </tr>
-                        )
-                        }
-            
-                        )
-            
-                        }
-            
-                    </table>
-                    </div> */}
       </MainRightPane>
     </div>
   );
